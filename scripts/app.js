db6c5364571d6b3e298d140122751ab06cbd7674
pmendoza72 <pablo_mendoza@hotmail.com>
2017-04-15 17:05:06 -0700

    added menu, data and js

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,10 @@
 'use strict'
 
-var projects = [
-  {
-    projectName: 'Salmon Cookies',
-    projectUrl: 'https://pmendoza72.github.io/cookie-stand/',
-    projectImageUrl: 'img/salmon-cookie-tn.png',
-    projectDescription: 'HTML table dynamically populated using JavaScript including form that adds to table.'
-  },
-  {
-    projectName: 'BlackJack',
-    projectUrl: 'https://pmendoza72.github.io/201-Project/',
-    projectImageUrl: 'img/blackjack-tn.png',
-    projectDescription: 'BlackJack card game using HTML, CSS and JavaScript only.'
-  },
-  {
-    projectName: 'Project 3',
-    projectUrl: '#',
-    projectImageUrl: 'http://placehold.it/300x300',
-    projectDescription: 'Project coming soon.'
-  },
-];
+var projectSource = $('#project-template').html();
+var handleBarsTemplate = Handlebars.compile(projectSource);
 
-  var projectSource = $('#project-template').html();
-  var handleBarsTemplate = Handlebars.compile(projectSource);
+// Pass our data to the template
+var theCompiledHtml = handleBarsTemplate(context);
 
-  for (var i in projects) {
-      var showProjects = handleBarsTemplate(projects[i]);
-      $('#insert-projects').append(showProjects);
-  };
+// Add the compiled html to the page
+$('#insert-projects').append(theCompiledHtml);
---
+++ scripts/data.js
@@ -0,0 +1,23 @@
+// This is the default context, which is passed to the template
+  var context = {
+    projects: [
+      {
+		projectName: 'Salmon Cookies',
+		projectUrl: 'https://pmendoza72.github.io/cookie-stand/',
+		projectImageUrl: 'img/salmon-cookie-tn.png',
+		projectDescription: 'HTML table dynamically populated using JavaScript including form that adds to table.'
+  	  },
+      {
+		projectName: 'BlackJack',
+		projectUrl: 'https://pmendoza72.github.io/201-Project/',
+		projectImageUrl: 'img/blackjack-tn.png',
+		projectDescription: 'BlackJack card game using HTML, CSS and JavaScript only.'
+  	  },
+      {
+		projectName: 'Project 3',
+		projectUrl: '#',
+		projectImageUrl: 'http://placehold.it/300x300',
+		projectDescription: 'Project coming soon.'
+  	  }
+    ]
+  };
