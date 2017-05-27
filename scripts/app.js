875b09c18392917b349b1aae0f7080391e2e8290
pmendoza72 <pablo_mendoza@hotmail.com>
2017-05-26 23:30:09 -0700

    ajax used to display data

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,26 @@
-'use strict'
+// 'use strict'
 
-var projectSource = $('#project-template').html();
-var handleBarsTemplate = Handlebars.compile(projectSource);
+// var projectSource = $('#project-template').html();
+// var handleBarsTemplate = Handlebars.compile(projectSource);
+//
+// // Pass our data to the template
+// var theCompiledHtml = handleBarsTemplate(context);
+//
+// // Add the compiled html to the page
+// $('#insert-projects').append(theCompiledHtml);
 
-// Pass our data to the template
-var theCompiledHtml = handleBarsTemplate(context);
 
-// Add the compiled html to the page
-$('#insert-projects').append(theCompiledHtml);
+$(() => {
+  $.ajax({
+    url: './data/projects.json'
+  }).done(function(data) {
+    console.log('request done: ' + Date.now());
+    data.forEach((note) => {
+      $('#insert-projects').append(`<article class="format-article"><a href="${note.projectUrl}"><h3>${note.projectName}</h3>
+        <img src="${note.projectImageUrl}" alt="${note.projectName}"></a>
+        <p>${note.projectDescription}</p>
+      </article>`);
+    });
+  });
+  console.log('request started: ' + Date.now());
+});
